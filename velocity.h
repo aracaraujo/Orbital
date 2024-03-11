f2c3a9c8a9a2949e0a5e4e230476db871cd67604
Ara Cantanhede Araujo <aracaraujo@Aras-MBP.home>
2024-03-11 15:18:19 -0700

    implementing velocity class

--- velocity.h
+++ velocity.h
@@ -12,19 +12,34 @@ public:
 
     // Constructors
     Velocity() : dy(0.0), dx(0.0) {};
-    Velocity(double dy, double dx) : dy(0.0), dx(0.0) {};
+    Velocity(double dy, double dx) : dy(dy), dx(dx) {};
 
     // Operators overload
-    Velocity& operator+=(const Velocity & rhs) {return *this;}
-    Velocity& operator-=(const Velocity & rhs) {return *this;}
-
+    Velocity& operator+=(const Velocity & rhs)
+    {
+        this->dy += rhs.dy;
+        this->dx += rhs.dx;
+        return *this;
+    }
+    Velocity& operator-=(const Velocity & rhs)
+    {
+        this->dy -= rhs.dy;
+        this->dx -= rhs.dx;
+        return *this;
+    }
     // Getters
-    double getVerticalVelocity() {return 0.0;};
-    double getHorizontalVelocity() {return 0.0;};
+    double getVerticalVelocity() {return this->dy;};
+    double getHorizontalVelocity() {return this->dx;};
 
     // Setters
-    void setVerticalVelocity(double dy) {};
-    void setHorizontalVelocity(double dx) {};
+    void setVerticalVelocity(double dy)
+    {
+        this->dy = dy;
+    };
+    void setHorizontalVelocity(double dx)
+    {
+        this->dx = dx;
+    };
 
     void updateVelocity(double ddy, double ddx, double time) {};
 
